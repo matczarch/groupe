ca6cd78ccae607de4c655744fff9c33531d75471
matczarch <matteo.richard95@gmail.com>
2021-10-10 22:54:39 +0200

    Ajout du front-end

---
+++ front-end/my-first-vue-cli-app/src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+    <header>
+    <img src="../assets/logo.png" alt="Logo provisoire"/>
+        <nav>
+        <ul>
+            <li v-for="(navItem, index) in navItems" v-bind:key="index"><router-link :to="navItem.link"> {{ navItem.title }}</router-link></li>
+        </ul>
+
+        </nav>
+        <h2> {{ pageTitle }} </h2>
+    </header>
+</template>
+
+<script> 
+    export default 
+    {
+        name: "Header",
+        components: {}, 
+        data(){
+            return {
+                pageTitle: "", 
+                navItems: [{
+                    title: "Acceuil", 
+                    link: "/",
+                },
+                {
+                    title: "A Propos", 
+                    link: "/about",
+                },
+                {
+                    title: "Connexion",
+                    link: "/Login",
+                }],
+                pageSubTitle: "Super Sous-titre", 
+            }
+        }, 
+    }
+</script>
+
+<style lang="scss"> 
+    header 
+    {
+        border: 1px solid black;
+        ul 
+        {
+            display:flex;
+            height: 5em; 
+            width: 10em; 
+            justify-content: space-around;
+            vertical-align: center; 
+            align-items: center; 
+            border: 1px solid black; 
+            li 
+            {
+                list-style-type: none;
+                border: 1px solid red;
+            }
+        }
+        display: flex;
+        justify-content: space-between; 
+        img 
+        {
+            width: 25%;
+        }
+
+    }
+</style>
---
+++ front-end/my-first-vue-cli-app/src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+    <Header/>
+    <div class="contain">
+        <h1> Se connecter </h1>
+    </div>
+    <div class="contain">
+        <p>Connectez-vous pour publier de nouveaux Posts</p>
+    </div>
+    <div class="form">
+        <label for="emailLogin">Adresse Email</label>
+        <input type="text" placeholder="Adresse mail" name="email" id="emailLogin"/> 
+    </div>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+
+</style> 
